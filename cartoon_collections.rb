494c4a8dc5d6b9f9fdbdfc718891fc30fce76df8
Domenico Cascone <dcascone84@gmail.com>
2020-03-12 01:24:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array_of_dwarf_names)
+    array_of_dwarf_names.each.with_index(1) { |name, index| puts "#{index}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |name| name.capitalize + "!"  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  long_planeteer_calls.any? { |name| name.length > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  if array_of_strings.include?(cheese_types)
+    cheese_types
+    
+  else
+    nil
+  end
 end
