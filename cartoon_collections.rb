8609792b19b3a651dd84f1257691e8505a7c20ce
Domenico Cascone <dcascone84@gmail.com>
2020-03-12 01:45:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find { |cheese| cheese = cheese_types }
+  snacks.find { |cheese| cheese == cheese_types }
 end
